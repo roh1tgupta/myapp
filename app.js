2614cfcb761f43026d0b366269a33fcd3656900d
Rohit Gupta <rohitgup@cybage.com>
2018-06-11 12:47:09 +0530

    middleware

--- app.js
+++ app.js
@@ -14,13 +14,39 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//will be executed for all request
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
+})
+
+//will be executed only for /users
+app.use('/users', function (req, res, next) {
+  console.log('Request URL:', req.originalUrl)
+  next()
+}, function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+})
+
 app.use('/', indexRouter);
+
+//adding middleware--practice and will be logged for /user and /getfile and not for / bcz middleware is defined after /
+var myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger);
+
 app.use('/users', usersRouter);
 app.use('/getfile', getfileRouter);
 
@@ -28,6 +54,7 @@ app.use('/getfile', getfileRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/:id', function(req, res, next) {
+  console.log('id  = ', req.params.id);
+  res.send('respond with a resource');
+});
+
 router.get('/', function(req, res, next) {
+  console.log('id  = null');
   res.send('respond with a resource');
 });
 
